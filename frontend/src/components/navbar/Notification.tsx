177750ef239d7442388f04c8483772526b5fe44f
deependu <deependujha21@gmail.com>
2024-09-29 16:58:49 +0530

    update

--- frontend/src/components/navbar/Notification.tsx
+++ frontend/src/components/navbar/Notification.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 
 const Notification = () => {
@@ -10,14 +10,18 @@ const Notification = () => {
     if ( !notificationVisible ) {
         return null;
     }
-  return (
-      <div className='bg-gradient-to-r from-indigo-500 via-purple-800 to-pink-500 flex justify-center'>
-          <div className='text-center text-white font-bold text-sm'>New! LitServe with gRPC support is now available.</div>
-          <div className='cursor-pointer ml-7' onClick={handleNotificationClose}>
-              <Image src="/images/cross-button.png" width={20} height={20} alt="cross-button"/>
-          </div>
-    </div>
-  )
+    return (
+        <div className='bg-gradient-to-r from-indigo-500 via-purple-800 to-pink-500 flex justify-center'>
+            <div className='text-center text-white text-sm my-1'>
+                <span className='font-bold'>
+                    New! LitServe with gRPC { ' ' }</span>
+                <span>support is now available.</span>
+            </div>
+            <div className='cursor-pointer ml-7 my-1' onClick={ handleNotificationClose }>
+                <Image src="/images/cross-button.png" width={ 20 } height={ 20 } alt="cross-button" />
+            </div>
+        </div>
+    )
 }
 
 export default Notification
