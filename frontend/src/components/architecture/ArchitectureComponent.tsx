16cd7cd1c069722293250d3acf5e483d75212fce
deependu <deependujha21@gmail.com>
2024-09-30 18:35:36 +0530

    architecture component done

--- frontend/src/components/architecture/ArchitectureComponent.tsx
+++ frontend/src/components/architecture/ArchitectureComponent.tsx
@@ -9,34 +9,16 @@ const ArchitectureComponent = () => {
   return (
     <div className="">
       <Accordion variant="bordered">
-        <AccordionItem
-          key="1"
-          aria-label="Accordion 1"
-          subtitle="Press to expand"
-          title="Accordion 1"
-        >
-          <div>{defaultContent}</div>
-        </AccordionItem>
-        <AccordionItem
-          key="2"
-          aria-label="Accordion 2"
-          subtitle={
-            <span>
-              Press to expand <strong>key 2</strong>
-            </span>
-          }
-          title="Accordion 2"
-        >
-          {defaultContent}
-        </AccordionItem>
-        <AccordionItem
-          key="3"
-          aria-label="Accordion 3"
-          subtitle="Press to expand"
-          title="Accordion 3"
-        >
-          {defaultContent}
-        </AccordionItem>
+        {[...Array(10)].map((_, i) => (
+          <AccordionItem
+            key={i}
+            aria-label={`Accordion ${i + 1}`}
+            subtitle="Press to expand"
+            title={`Accordion ${i + 1}`}
+          >
+            <div>{defaultContent}</div>
+          </AccordionItem>
+        ))}
       </Accordion>
     </div>
   );
